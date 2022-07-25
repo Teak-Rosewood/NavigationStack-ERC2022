a06b282da512c45c32ce218ba4d4f479463893c0
Saatwik V <saatwik.vasishtha@gmail.com>
2022-07-25 16:24:51 +0530

    updated costmap

--- src/rover/src/GPE.cpp
+++ src/rover/src/GPE.cpp
@@ -18,6 +18,7 @@
 #include <pcl/features/normal_3d.h>
 
 #include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/region_growing_rgb.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/statistical_outlier_removal.h>
@@ -42,12 +43,13 @@ public:
 
 private:
     // Preprocessing of data
-    void preprocessing(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_processing);
+    void preprocessing(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_processing);
     // Ground Plane Elimination
-    void ground_plane_elimination(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_processing);
+    void ground_plane_elimination(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_processing);
     // Clustering
-    void clustering(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_processing);
+    void clustering(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_processing);
     sensor_msgs::PointCloud2 data;
+    pcl::PointCloud<pcl::PointXYZRGB> cloud_final;
 };
 
 void segmentation::cloud_callback(const sensor_msgs::PointCloud2ConstPtr &PointCloud2)
@@ -55,7 +57,7 @@ void segmentation::cloud_callback(const sensor_msgs::PointCloud2ConstPtr &PointC
 
     // PCL and PointCloud2 Point cloud
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
     pcl::PCLPointCloud2 cloud_pcl2;
     sensor_msgs::PointCloud2 pointcloud2;
 
@@ -65,161 +67,78 @@ void segmentation::cloud_callback(const sensor_msgs::PointCloud2ConstPtr &PointC
     pcl::fromPCLPointCloud2(cloud_pcl2, *cloud);
     preprocessing(cloud);
     ground_plane_elimination(cloud);
-    pcl::toROSMsg(*cloud, data);
+    pcl::toROSMsg(cloud_final, data);
     data.header.frame_id = "camera_optical_frame";
 
     pub.publish(data);
 }
 
-void segmentation::preprocessing(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_processing)
+void segmentation::preprocessing(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_processing)
 {
     // Pass through filter
     
-    pcl::PassThrough<pcl::PointXYZ> pass;
+    pcl::PassThrough<pcl::PointXYZRGB> pass;
     pass.setInputCloud(cloud_processing);
     pass.setFilterFieldName("z");
     pass.setFilterLimits(0.5, 3);
     pass.filter(*cloud_processing);
 
-    pcl::PassThrough<pcl::PointXYZ> pass2;
+    pcl::PassThrough<pcl::PointXYZRGB> pass2;
     pass2.setInputCloud(cloud_processing);
     pass2.setFilterFieldName("x");
     pass2.setFilterLimits(-1.5,1.5);
     pass2.setFilterLimitsNegative (false);
     pass2.filter(*cloud_processing);
-    /*
-    pcl::PassThrough<pcl::PointXYZ> pass2;
-    pass2.setInputCloud(cloud_processing);
-    pass2.setFilterFieldName("y");
-    pass2.setFilterLimits(0.01, 10);
-    pass2.setFilterLimitsNegative (true);
-    pass2.filter(*cloud_processing);
 
-    /*
-    pcl::PassThrough<pcl::PointXYZRGB> pass3;
-    pass3.setInputCloud(cloud_processing);
-    pass3.setFilterFieldName("x");
-    pass3.setFilterLimits(3,100000);
-    
-    pass3.filter(*cloud_processing);
-    */
-    // Voxelization.
 
     pcl::PCLPointCloud2::Ptr voxelization(new pcl::PCLPointCloud2());
     pcl::toPCLPointCloud2(*cloud_processing, *voxelization);
 
     pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
     sor.setInputCloud(voxelization);
-    sor.setLeafSize(0.05f, 0.05f, 0.05f);
+    sor.setLeafSize(0.04f, 0.04f, 0.04f);
     sor.filter(*voxelization);
     pcl::fromPCLPointCloud2(*voxelization, *cloud_processing);
 }
 
-void segmentation::ground_plane_elimination(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_processing)
+void segmentation::ground_plane_elimination(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_processing)
 {
-    // // Normal Estimation (NOT IN USE CURRENTLY)
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_f(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::search::Search <pcl::PointXYZRGB>::Ptr tree2 (new pcl::search::KdTree<pcl::PointXYZRGB>);
+    pcl::IndicesPtr indices (new std::vector <int>);
+    pcl::PassThrough<pcl::PointXYZRGB> pass;
+    pass.setInputCloud (cloud_processing);
+    pass.setFilterFieldName ("z");
+    pass.setFilterLimits (0.2, 3.0);
+    pass.filter (*indices);
 
-    // pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> ne;
-    // ne.setInputCloud (cloud_processing);
+    pcl::RegionGrowingRGB<pcl::PointXYZRGB> reg;
+    reg.setInputCloud (cloud_processing);
+    reg.setIndices (indices);
+    reg.setSearchMethod (tree2);
+    reg.setDistanceThreshold (50);
+    reg.setPointColorThreshold (11);
+    reg.setRegionColorThreshold (11);
+    reg.setMinClusterSize (25);
+    reg.setMaxClusterSize(100000);
 
-    // pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB> ());
-    // ne.setSearchMethod (tree);
-
-    // // Output datasets
-    // pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
-
-    // ne.setRadiusSearch (0.04);
-
-    // // Compute the features
-    // ne.compute (*cloud_normals);
-
-    // Plane segmentation using RANSAC
-    
-
-    pcl::PointIndices::Ptr indices (new pcl::PointIndices ());
-    std::vector<int> inliers;
-
-    pcl::SampleConsensusModelPlane<pcl::PointXYZ>::Ptr
-    model_p (new pcl::SampleConsensusModelPlane<pcl::PointXYZ> (cloud_processing));
-
-    pcl::RandomSampleConsensus<pcl::PointXYZ> ransac (model_p);
-    ransac.setDistanceThreshold (0.4);
-    ransac.computeModel();
-    ransac.getInliers(inliers);
-
-    indices->indices = inliers;
-
-    pcl::ExtractIndices<pcl::PointXYZ> extract;
-    extract.setInputCloud(cloud_processing);
-    extract.setIndices(indices);
-    extract.setNegative(true);
-    extract.filter(*cloud_processing);
-
-
-   /*
-    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-    // Create the segmentation object
-    pcl::SACSegmentation<pcl::PointXYZ> seg;
-    // Optional
-    seg.setOptimizeCoefficients (true);
-    // Mandatory
-    seg.setModelType (pcl::SACMODEL_PLANE);
-    seg.setMethodType (pcl::SAC_RANSAC);
-    seg.setDistanceThreshold (0.1);
-
-    seg.setInputCloud (cloud_processing);
-    seg.segment (*inliers, *coefficients);
-    */
-    pcl::PassThrough<pcl::PointXYZ> pass;
-    pass.setInputCloud(cloud_processing);
-    pass.setFilterFieldName("z");
-    pass.setFilterLimits(0.5, 3.0);
-    pass.filter(*cloud_processing);
-    
-    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
-    ne.setInputCloud (cloud_processing);
-    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
-    ne.setSearchMethod (tree);
-    pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
-    ne.setRadiusSearch (0.5);
-    ne.compute (*cloud_normals);
-    
-    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree2 (new pcl::search::KdTree<pcl::PointXYZ>);
-    tree2->setInputCloud(cloud_processing);
-
-    std::vector<pcl::PointIndices> cluster_indices;
-    pcl::EuclideanClusterExtraction<pcl::PointXYZ> Euclidean_cluster;
-    Euclidean_cluster.setClusterTolerance (0.01); // 2cm
-    Euclidean_cluster.setMinClusterSize (50);
-    Euclidean_cluster.setMaxClusterSize (20000);
-    Euclidean_cluster.setSearchMethod (tree2);
-    Euclidean_cluster.setInputCloud(cloud_processing);
-    Euclidean_cluster.extract(cluster_indices);
+    std::vector <pcl::PointIndices> cluster_indices;
+    reg.extract (cluster_indices);
 
     for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
     {
-      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
+      pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZRGB>);
       for (const auto& idx : it->indices){
         cloud_cluster->push_back ((*cloud_processing)[idx]); //*
       }
        cloud_cluster->width = cloud_cluster->size ();
        cloud_cluster->height = 1;
        cloud_cluster->is_dense = true;
-       *cloud_processing += *cloud_cluster;
+       if(cloud_cluster->size ()<500){
+         *cloud_f += *cloud_cluster;
+       }
      }
-    pcl::PassThrough<pcl::PointXYZ> pass3;
-    pass3.setInputCloud(cloud_processing);
-    pass3.setFilterFieldName("z");
-    pass3.setFilterLimits(0.5, 3);
-    pass3.filter(*cloud_processing);
-
-    pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
-    sor.setInputCloud (cloud_processing);
-    sor.setMeanK (50);
-    sor.setStddevMulThresh (1.0);
-    sor.filter (*cloud_processing);
-    
+     cloud_final = *cloud_f;
 }
 
 
--- src/rover/src/movebaseGoal.cpp
+++ src/rover/src/movebaseGoal.cpp
@@ -1,26 +1,61 @@
 #include "ros/ros.h"
 #include "geometry_msgs/PoseStamped.h"
+#include "nav_msgs/Odometry.h"
+#include <std_msgs/Empty.h>
 #include <iostream>
+#include <cmath>
+class ProbeDeploy{
+    public:
+        double value_x=0;
+        double value_y=0;
+        double orientZ=0;
+        double orientW=0;
+        void OdomCallback(const nav_msgs::Odometry::ConstPtr& odom);
+    private:
+        
+};
+void ProbeDeploy::OdomCallback(const nav_msgs::Odometry::ConstPtr& odom){
+    value_x = odom->pose.pose.position.x;
+    value_y = odom->pose.pose.position.y;
+}
+double dist(double x1,double x2,double y1,double y2){
+    return std::sqrt((y2-y1)*(y2-y1)+(x2-x1)*(x2-x1));
+}
 
 int main(int argc,char **argv){
-    float x_pose,y_pose,z_pose,x,y,z,w;
-    std::cout<<"Enter Pose(x,y): "<<std::endl;
-    std::cin>>x_pose>>y_pose;
-    std::cout<<"Enter Orientation(z,w): "<<std::endl;
-    std::cin>>z>>w;
+
+    ProbeDeploy odom;
+    double X,Y,distance;
+    bool IsScience;
+    std_msgs::Empty EMsg;
+    geometry_msgs::PoseStamped msg;
+    std::cout<<"Enter goal location(x,y,science):"<<std::endl;
+    std::cin>>X>>Y>>IsScience;
+    msg.pose.position.x = X;
+    msg.pose.position.y = Y;
+    msg.pose.orientation.x=0;
+    msg.pose.orientation.y=0;
+    msg.pose.orientation.z=1;
+    msg.pose.orientation.w=0;
+    msg.header.frame_id="map";
     ros::init(argc,argv,"movebase_goal");
     ros::NodeHandle node;
-    ros::Publisher pub = node.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal",10);
-    geometry_msgs::PoseStamped msg;
-    msg.pose.position.x = x_pose;
-    msg.pose.position.y = y_pose;
-    msg.pose.orientation.z = z;
-    msg.pose.orientation.w = w;
-    msg.header.frame_id="map";
-    ros::Rate rate(10);
-    while(ros::ok()){
+    ros::Publisher pub = node.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal",10);
+    ros::Publisher pub2 = node.advertise<std_msgs::Empty>("/probe_deployment_unit/drop",1);
+    ros::Subscriber sub = node.subscribe<nav_msgs::Odometry>("/odometry/filtered",10,&ProbeDeploy::OdomCallback,&odom);
+    ros::Rate r(10);
+    do{
+        distance = dist(odom.value_x,X,odom.value_y,Y);
+        if(distance<0.4){
+            if(IsScience==0){
+                pub2.publish(EMsg);
+            }            
+            std::cout<<"Enter next goal location(x,y,science):"<<std::endl;
+            std::cin>>msg.pose.position.x>>msg.pose.position.y>>IsScience;
+        }
         pub.publish(msg);
         ros::spinOnce();
-        rate.sleep();
-    }
+        r.sleep();
+        
+    }while(ros::ok());
 }
